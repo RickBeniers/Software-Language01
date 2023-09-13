defdab8ea9e39629109eccaf6a425eafa4da58fe
RickBeniers <Beniersrick@gmail.com>
2023-09-13 14:12:28 +0200

    started working on tasks from college 2. 13-09-2023, 1412.

--- Index.php
+++ Index.php
@@ -4,15 +4,17 @@
 <html>
     <head>
         <title>Opdracht W01P01</title>
-        <link rel="stylesheet" href="IndexStyle.css?v=3">
+        <link rel="stylesheet" href="IndexStyle.css?v=4">
     </head>
     <body>
         <header class="header"></header>
         <main>
             <div class="contentDiv">
                 <?php
-                    DisplayInfo();
+                    //call methods
+                    //DisplayInfo();
                     CheckTimeOfDay();
+                    PrintArray();
                 ?>
             </div>
         </main>
--- IndexFunctions.php
+++ IndexFunctions.php
@@ -5,6 +5,11 @@ this document is meant to be shared with the teachers of the study.
 
 This document is made by Rick Beniers, s1155108. 06-09-2023. -->
     <?php
+    //declare and initialise variables
+    $lid_1 = [];
+    $lid_2 = [];
+    $lid_3 = [];
+    $groep = [];
     // Single line comment
 
     /*
@@ -53,8 +58,9 @@ This document is made by Rick Beniers, s1155108. 06-09-2023. -->
             echo "<br>".$familie[$i];
         }
     }
+
     /**
-     * Display the data from ten variables.
+     * Change the background color based on the time of day.
      */
     function CheckTimeOfDay(){
         //if the time in hours is between 0600 and 1200
@@ -72,5 +78,69 @@ This document is made by Rick Beniers, s1155108. 06-09-2023. -->
             //change background color to dark grey
             ?> <style> .contentDiv{background-color: darkgray}</style> <?php
         }
+    }
+
+    /**
+     * Echo the content of the multidimensional array
+     */
+    function PrintArray()
+    {
+        //voeg data toe aan persoon 1
+        global $lid_1;
+        AddDataToArray("Naam:", "Rick Beniers", $lid_1);
+        AddDataToArray("Leeftijd:", 26, $lid_1);
+        AddDataToArray("Geboren op:", "11-05-1997", $lid_1);
+        AddDataToArray("Heeft relatie:", false, $lid_1);
+        AddDataToArray("Lengte:", 182, $lid_1);
+        AddDataToArray("Email:", "Beniersrick@gmail.com", $lid_1);
+        AddDataToArray("Mobiel:","0620297864", $lid_1);
+        AddDataToArray("Werkt als:", "Secretaris bij BoKS", $lid_1);
+
+        //voeg data toe aan persoon 2
+        global $lid_2;
+        AddDataToArray("Naam:", "Paul", $lid_2);
+        AddDataToArray("Leeftijd:", 20, $lid_2);
+        AddDataToArray("Gbeoren op:", "99-99-9999", $lid_2);
+        AddDataToArray("Heeft relatie:", false, $lid_2);
+        AddDataToArray("Lengte:",180, $lid_2);
+        AddDataToArray("Email:", "?@?", $lid_2);
+        AddDataToArray("Mobiel:", "9999999999", $lid_2);
+        AddDataToArray("Werkt als:", "Onbekend", $lid_2);
+
+        //voeg data toe aan persoon 3
+        global $lid_3;
+        AddDataToArray("Naam:", "Sander", $lid_3);
+        AddDataToArray("Leeftijd:",22, $lid_3);
+        AddDataToArray("Gbeoren op:", "99-99-9999", $lid_3);
+        AddDataToArray("Heeft relatie:", false, $lid_3);
+        AddDataToArray("Lengte:", 179, $lid_3);
+        AddDataToArray("Email:", "?@?", $lid_3);
+        AddDataToArray("Mobiel:", "9999999999", $lid_3);
+        AddDataToArray("Werkt als:", "Onbekend", $lid_3);
+
+        //voeg array van persoon toe aan groep
+        global $groep;
+        AddDataToArray("Rick", $lid_1, $groep);
+        AddDataToArray("Paul", $lid_2, $groep);
+        AddDataToArray("Sander", $lid_3, $groep);
+
+        //print array
+        for($row=0; $row < count($groep);$row++){
+            echo "<p><b>Row number $row</b></p>";
+            echo "<ul>";
+            for($col=0; $col < count($groep[$col]); $col++){
+                echo "<li>".$groep[$row][$col]."</li>";
+            }
+            echo "</ul>";
+        }
+    }
+
+    /**
+     * Populate variable array lid with data
+     */
+    function AddDataToArray($index,$value, $array)
+    {
+        //add the parameter value to  the array
+        $array = [$index => $value];
     }
     ?>
